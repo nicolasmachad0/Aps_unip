a867cbd76990ccc4323a9451d16364d2b4f04152
Nicolas Machado <nicolas.machadofogaca@gmail.com>
2023-04-20 23:18:14 -0300

    Atualizando

--- scripts.js
+++ scripts.js
@@ -1,26 +1,26 @@
 function trocaTexto1() {
-    var texto = document.getElementById("meuTexto");
-    if (texto.innerHTML === "Texto inicial") {
-      texto.innerHTML = "O meio ambiente refere-se ao conjunto de fatores físicos, biológicos e químicos que cerca os seres vivos, influenciando-os e sendo influenciado por eles. Pode ser entendido também como o conjunto de condições que permitem abrigar e reger a vida em todas as suas formas - os ecossistemas que existem na Terra. Wikipédia";
-    } else {
-      texto.innerHTML = "O meio ambiente refere-se ao conjunto de fatores físicos, biológicos e químicos que cerca os seres vivos, influenciando-os e sendo influenciado por eles. Pode ser entendido também como o conjunto de condições que permitem abrigar e reger a vida em todas as suas formas - os ecossistemas que existem na Terra. Wikipédia";
-    }
+  var texto = document.getElementById("meuTexto");
+  if (texto.innerHTML === "Texto inicial") {
+    texto.innerHTML = "O meio ambiente refere-se ao conjunto de fatores físicos, biológicos e químicos que cerca os seres vivos, influenciando-os e sendo influenciado por eles. Pode ser entendido também como o conjunto de condições que permitem abrigar e reger a vida em todas as suas formas - os ecossistemas que existem na Terra. Wikipédia";
+  } else {
+    texto.innerHTML = "O meio ambiente refere-se ao conjunto de fatores físicos, biológicos e químicos que cerca os seres vivos, influenciando-os e sendo influenciado por eles. Pode ser entendido também como o conjunto de condições que permitem abrigar e reger a vida em todas as suas formas - os ecossistemas que existem na Terra. Wikipédia";
   }
+}
 
-  function trocaTexto2() {
-    var texto = document.getElementById("meuTexto");
-    if (texto.innerHTML === ".") {
-      texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
-    } else {
-      texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
-    }
+function trocaTexto2() {
+  var texto = document.getElementById("meuTexto");
+  if (texto.innerHTML === ".") {
+    texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
+  } else {
+    texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
   }
+}
 
-  function trocaTexto3() {
-    var texto = document.getElementById("meuTexto");
-    if (texto.innerHTML === ".") {
-      texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
-    } else {
-      texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
-    }
+function trocaTexto3() {
+  var texto = document.getElementById("meuTexto");
+  if (texto.innerHTML === ".") {
+    texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
+  } else {
+    texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
   }
+}

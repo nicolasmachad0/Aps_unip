711bdc4774b634cb26191c9d601a380311ab573f
Nicolas Machado <nicolas.machadofogaca@gmail.com>
2023-04-26 17:13:12 -0300

    Atualizando

---
+++ scripts2.js
@@ -0,0 +1,27 @@
+function trocaTexto1() {
+    var texto = document.getElementById("meuTexto");
+    if (texto.innerHTML === "Texto inicial") {
+      texto.innerHTML = "O conceito de sustentabilidade começou a ser difundido pelo mundo logo após a Segunda Guerra Mundial, em função da crescente evolução tecnológica que se estendeu até a Guerra Fria, causando impactos como poluição atmosférica, poluição dos solos, desmatamento e esgotamento de recursos naturais.";
+    } else {
+      texto.innerHTML = "O conceito de sustentabilidade começou a ser difundido pelo mundo logo após a Segunda Guerra Mundial, em função da crescente evolução tecnológica que se estendeu até a Guerra Fria, causando impactos como poluição atmosférica, poluição dos solos, desmatamento e esgotamento de recursos naturais.";
+    }
+  }
+  
+  function trocaTexto2() {
+    var texto = document.getElementById("meuTexto");
+    if (texto.innerHTML === ".") {
+      texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
+    } else {
+      texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
+    }
+  }
+  
+  function trocaTexto3() {
+    var texto = document.getElementById("meuTexto");
+    if (texto.innerHTML === ".") {
+      texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
+    } else {
+      texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
+    }
+  }
+  

57ebb470db9c72f8a7ab3535d1a9c6112104102d
Nicolas Machado <nicolas.machadofogaca@gmail.com>
2023-04-26 18:47:21 -0300

    Atualizando

--- scripts2.js
+++ scripts2.js
@@ -1,27 +1,54 @@
 function trocaTexto1() {
     var texto = document.getElementById("meuTexto");
     if (texto.innerHTML === "Texto inicial") {
-      texto.innerHTML = "O conceito de sustentabilidade começou a ser difundido pelo mundo logo após a Segunda Guerra Mundial, em função da crescente evolução tecnológica que se estendeu até a Guerra Fria, causando impactos como poluição atmosférica, poluição dos solos, desmatamento e esgotamento de recursos naturais.";
+        texto.innerHTML = "O conceito de sustentabilidade começou a ser difundido pelo mundo logo após a Segunda Guerra Mundial, em função da crescente evolução tecnológica que se estendeu até a Guerra Fria, causando impactos como poluição atmosférica, poluição dos solos, desmatamento e esgotamento de recursos naturais.";
     } else {
-      texto.innerHTML = "O conceito de sustentabilidade começou a ser difundido pelo mundo logo após a Segunda Guerra Mundial, em função da crescente evolução tecnológica que se estendeu até a Guerra Fria, causando impactos como poluição atmosférica, poluição dos solos, desmatamento e esgotamento de recursos naturais.";
+        texto.innerHTML = "O conceito de sustentabilidade começou a ser difundido pelo mundo logo após a Segunda Guerra Mundial, em função da crescente evolução tecnológica que se estendeu até a Guerra Fria, causando impactos como poluição atmosférica, poluição dos solos, desmatamento e esgotamento de recursos naturais.";
     }
-  }
-  
-  function trocaTexto2() {
+}
+
+function trocaTexto2() {
     var texto = document.getElementById("meuTexto");
     if (texto.innerHTML === ".") {
-      texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
+        texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
     } else {
-      texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
+        texto.innerHTML = "Sustentabilidade é uma característica ou condição de um processo ou de um sistema que permite a sua permanência, em certo nível, por um determinado prazo.";
     }
-  }
-  
-  function trocaTexto3() {
+}
+
+function trocaTexto3() {
     var texto = document.getElementById("meuTexto");
     if (texto.innerHTML === ".") {
-      texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
+        texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
     } else {
-      texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
+        texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
     }
-  }
-  
+}
+
+
+function trocaTexto1() {
+    var texto = document.getElementById("meuTexto2");
+    if (texto.innerHTML === "Texto inicial") {
+        texto.innerHTML = "Desde fevereiro de 2020, a sede da Secretaria do Meio Ambiente e Sustentabilidade é ponto de entrega voluntária de lixo eletrônico em Sorocaba. O coletor metálico instalado na entrada da Secretaria do Meio Ambiente recebe resíduos com dimensões máximas de 30 cm de altura e 60 cm de largura. Lâmpadas fluorescentes não estão incluídas. Todo o material é recolhido e transportado por empresas certificadas e homologadas, que fazem a reciclagem e destinação correta do lixo eletrônico.";
+    } else {
+        texto.innerHTML = "Desde fevereiro de 2020, a sede da Secretaria do Meio Ambiente e Sustentabilidade é ponto de entrega voluntária de lixo eletrônico em Sorocaba. O coletor metálico instalado na entrada da Secretaria do Meio Ambiente recebe resíduos com dimensões máximas de 30 cm de altura e 60 cm de largura. Lâmpadas fluorescentes não estão incluídas. Todo o material é recolhido e transportado por empresas certificadas e homologadas, que fazem a reciclagem e destinação correta do lixo eletrônico.";
+    }
+}
+
+function trocaTexto2() {
+    var texto = document.getElementById("meuTexto2");
+    if (texto.innerHTML === ".") {
+        texto.innerHTML = "Este resultado foi percebido por meio da diminuição do consumo de energia, água e de materiais, notadamente os copos descartáveis e papel. Indiretamente, toda a sociedade é beneficiada. Estas ações foram realizadas mediante as decisões tomadas durante os encontros do Comitê de Sustentabilidade. Por meio das ações do programa, pode-se perceber, mediante os controles de gastos, acentuadas reduções concernentes ao uso de energia, água e materiais, fato que auxilia a redução dos impactos ambientais.";
+    } else {
+        texto.innerHTML = "Este resultado foi percebido por meio da diminuição do consumo de energia, água e de materiais, notadamente os copos descartáveis e papel. Indiretamente, toda a sociedade é beneficiada. Estas ações foram realizadas mediante as decisões tomadas durante os encontros do Comitê de Sustentabilidade. Por meio das ações do programa, pode-se perceber, mediante os controles de gastos, acentuadas reduções concernentes ao uso de energia, água e materiais, fato que auxilia a redução dos impactos ambientais.";
+    }
+}
+
+function trocaTexto3() {
+    var texto = document.getElementById("meuTexto2");
+    if (texto.innerHTML === ".") {
+        texto.innerHTML = "Sorocaba vem desenvolvendo nos últimos anos ações de sustentabilidade com implantação de energia fotovoltaica em prédios públicos municipais. Trata-se de uma das formas mais limpas de produção de energia e que mais cresce no mundo, composta por painéis que captam a energia do sol. Além de reduzir a emissão de gases do efeito estufa (GEE) e o consumo de energia, a medida sustentável ainda gera economia de recursos aos cofres públicos.";
+    } else {
+        texto.innerHTML = "Sorocaba vem desenvolvendo nos últimos anos ações de sustentabilidade com implantação de energia fotovoltaica em prédios públicos municipais. Trata-se de uma das formas mais limpas de produção de energia e que mais cresce no mundo, composta por painéis que captam a energia do sol. Além de reduzir a emissão de gases do efeito estufa (GEE) e o consumo de energia, a medida sustentável ainda gera economia de recursos aos cofres públicos.";
+    }
+}

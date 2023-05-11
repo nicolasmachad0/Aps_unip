f90cbdfe08b58e738dfcf33e6a20190bd3b63f16
Nicolas Machado <nicolas.machadofogaca@gmail.com>
2023-05-11 10:44:43 -0300

    Atualizando

--- scripts.js
+++ scripts.js
@@ -37,18 +37,18 @@ function trocaTexto4() {
 function trocaTexto5() {
   var texto = document.getElementById("meuTexto2");
   if (texto.innerHTML === ".") {
-      texto.innerHTML = "Em Sorocaba e região, são cidades que tendem aumentar mais e mais o consumo por conta grandes desenvolvimentos das empresas e industrias. ";
+      texto.innerHTML = "A compreensão da importância do papel da sustentabilidade no meio ambiente é fundamental e já está sendo colocada em prática pela população e grande parte das empresas, que buscam minimizar o impacto de suas ações na natureza por meio da reciclagem de seus materiais e a utilização de fontes renováveis de energia.";
   } else {
-      texto.innerHTML = "Em Sorocaba e região, são cidades que tendem aumentar mais e mais o consumo por conta grandes desenvolvimentos das empresas e industrias. ";
+      texto.innerHTML = "A compreensão da importância do papel da sustentabilidade no meio ambiente é fundamental e já está sendo colocada em prática pela população e grande parte das empresas, que buscam minimizar o impacto de suas ações na natureza por meio da reciclagem de seus materiais e a utilização de fontes renováveis de energia.";
   }
 }
 
 function trocaTexto6() {
   var texto = document.getElementById("meuTexto2");
   if (texto.innerHTML === ".") {
-      texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
+      texto.innerHTML = "Em Sorocaba e região, são cidades que tendem aumentar mais e mais o consumo por conta grandes desenvolvimentos das empresas e industrias. ";
   } else {
-      texto.innerHTML = "De acordo com o Ministério do Meio Ambiente, o consumo sustentável implica na escolha de produtos que: Utilizaram menos recursos naturais em sua produção. Garantam emprego decente aos que os produziram. São facilmente reaproveitados ou reciclados.";
+      texto.innerHTML = "Em Sorocaba e região, são cidades que tendem aumentar mais e mais o consumo por conta grandes desenvolvimentos das empresas e industrias. ";
   }
 }
 
@@ -56,26 +56,26 @@ function trocaTexto6() {
 function trocaTexto7() {
   var texto = document.getElementById("meuTexto3");
   if (texto.innerHTML === "Texto inicial") {
-      texto.innerHTML = "Desde fevereiro de 2020, a sede da Secretaria do Meio Ambiente e Sustentabilidade é ponto de entrega voluntária de lixo eletrônico em Sorocaba. Todo o material é recolhido e transportado por empresas certificadas e homologadas, que fazem a reciclagem e destinação correta do lixo eletrônico.";
+      texto.innerHTML = "Implemetada em 2020, a Sede da Secretaria do Meio Ambiente e Sustentabilidade é ponto de entrega voluntária de lixo eletrônico em Sorocaba. Todo o material é recolhido e transportado por empresas certificadas e homologadas, que fazem a reciclagem e destinação correta do lixo eletrônico.";
   } else {
-      texto.innerHTML = "Desde fevereiro de 2020, a sede da Secretaria do Meio Ambiente e Sustentabilidade é ponto de entrega voluntária de lixo eletrônico em Sorocaba. Todo o material é recolhido e transportado por empresas certificadas e homologadas, que fazem a reciclagem e destinação correta do lixo eletrônico.";
+      texto.innerHTML = "Implemetada em 2020, a Sede da Secretaria do Meio Ambiente e Sustentabilidade é ponto de entrega voluntária de lixo eletrônico em Sorocaba. Todo o material é recolhido e transportado por empresas certificadas e homologadas, que fazem a reciclagem e destinação correta do lixo eletrônico.";
   }
 }
 
 function trocaTexto8() {
   var texto = document.getElementById("meuTexto3");
   if (texto.innerHTML === ".") {
-      texto.innerHTML = "Este resultado foi percebido por meio da diminuição do consumo de energia, água e de materiais, notadamente os copos descartáveis e papel. Indiretamente, toda a sociedade é beneficiada. Estas ações foram realizadas mediante as decisões tomadas durante os encontros do Comitê de Sustentabilidade. Por meio das ações do programa, pode-se perceber, mediante os controles de gastos, acentuadas reduções concernentes ao uso de energia, água e materiais, fato que auxilia a redução dos impactos ambientais.";
+      texto.innerHTML = "No hospital Unimed de Sorocaba, este resultado foi percebido por meio da diminuição do consumo de energia, água e de materiais, notadamente os copos descartáveis e papel. Indiretamente, toda a sociedade é beneficiada. Por meio das ações do programa, pode-se perceber, mediante os controles de gastos, acentuadas reduções ao uso de energia, água e materiais, fato que auxilia a redução dos impactos ambientais.";
   } else {
-      texto.innerHTML = "Este resultado foi percebido por meio da diminuição do consumo de energia, água e de materiais, notadamente os copos descartáveis e papel. Indiretamente, toda a sociedade é beneficiada. Estas ações foram realizadas mediante as decisões tomadas durante os encontros do Comitê de Sustentabilidade. Por meio das ações do programa, pode-se perceber, mediante os controles de gastos, acentuadas reduções concernentes ao uso de energia, água e materiais, fato que auxilia a redução dos impactos ambientais.";
+      texto.innerHTML = "No hospital Unimed de Sorocaba, este resultado foi percebido por meio da diminuição do consumo de energia, água e de materiais, notadamente os copos descartáveis e papel. Indiretamente, toda a sociedade é beneficiada. Por meio das ações do programa, pode-se perceber, mediante os controles de gastos, acentuadas reduções ao uso de energia, água e materiais, fato que auxilia a redução dos impactos ambientais.";
   }
 }
 
 function trocaTexto9() {
   var texto = document.getElementById("meuTexto3");
   if (texto.innerHTML === ".") {
-      texto.innerHTML = "Sorocaba vem desenvolvendo nos últimos anos ações de sustentabilidade com implantação de energia fotovoltaica em prédios públicos municipais. Trata-se de uma das formas mais limpas de produção de energia e que mais cresce no mundo, composta por painéis que captam a energia do sol. Além de reduzir a emissão de gases do efeito estufa (GEE) e o consumo de energia, a medida sustentável ainda gera economia de recursos aos cofres públicos.";
+      texto.innerHTML = "Sorocaba vem desenvolvendo nos últimos anos ações de sustentabilidade com implantação de energia fotovoltaica em prédios públicos municipais. Trata-se de uma das formas mais limpas de produção de energia e que mais cresce no mundo, composta por painéis que captam a energia do sol. Além de reduzir a emissão de gases do efeito estufa e o consumo de energia, a medida sustentável ainda gera economia de recursos aos cofres públicos.";
   } else {
-      texto.innerHTML = "Sorocaba vem desenvolvendo nos últimos anos ações de sustentabilidade com implantação de energia fotovoltaica em prédios públicos municipais. Trata-se de uma das formas mais limpas de produção de energia e que mais cresce no mundo, composta por painéis que captam a energia do sol. Além de reduzir a emissão de gases do efeito estufa (GEE) e o consumo de energia, a medida sustentável ainda gera economia de recursos aos cofres públicos.";
+      texto.innerHTML = "Sorocaba vem desenvolvendo nos últimos anos ações de sustentabilidade com implantação de energia fotovoltaica em prédios públicos municipais. Trata-se de uma das formas mais limpas de produção de energia e que mais cresce no mundo, composta por painéis que captam a energia do sol. Além de reduzir a emissão de gases do efeito estufa e o consumo de energia, a medida sustentável ainda gera economia de recursos aos cofres públicos.";
   }
 }
